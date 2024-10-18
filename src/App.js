d5f597a6373b2e390b1955758dde094b3e0edbe4
emily <emilyhou8338@gmail.com>
2024-10-18 18:36:05 +0800

    feat: setup addComma and getNumberIntervals func.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { addComma, getNumberIntervals } from './FunctionList';
 
 function App() {
+  const [array, setArray] = useState([[], [], [], []]);
+  const addArray = (val, index, position) => {
+    console.log(val, index, position);
+    const rs = [...array];
+    if (position === 'start') rs.splice(index, 1, [val, array[index][1] || 20]);
+    if (position === 'end') rs.splice(index, 1, [array[index][0] || 0, val]);
+    setArray(rs);
+  }
+
+  useEffect(() => {
+    console.log(array, typeof array, 'inside useEffect');
+    const res = getNumberIntervals(array);
+    console.log('after running: ', res);
+  }, [array]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>AsiaYo PreTest Display page</p>
+        {/* <input onChange={addComma}></input> */}
+        <input onChange={(e) => addComma(e.target.value)}></input>
+        <hr />
+        <div style={{ display: 'flex' }}>
+          <input onChange={(e) => addArray(e.target.value, 0, 'start')} />
+          <input onChange={(e) => addArray(e.target.value, 0, 'end')} />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <input onChange={(e) => addArray(e.target.value, 1, 'start')} />
+          <input onChange={(e) => addArray(e.target.value, 1, 'end')} />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <input onChange={(e) => addArray(e.target.value, 2, 'start')} />
+          <input onChange={(e) => addArray(e.target.value, 2, 'end')} />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <input onChange={(e) => addArray(e.target.value, 3, 'start')} />
+          <input onChange={(e) => addArray(e.target.value, 3, 'end')} />
+        </div>
       </header>
     </div>
   );
---
+++ src/FunctionList.js
@@ -0,0 +1,51 @@
+export function addComma(input) {
+  // expect output > num with thousand period comma;
+  const num = Number(input);
+  if (isNaN(num)) return input;
+  const splitNum = num.toString().split('.');
+  const int = splitNum[0];
+  const deci = splitNum[1];
+  const intWithComma = int.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const result = `${intWithComma}.${deci}`;
+  return result;
+};
+
+export function getNumberIntervals(intervals) {
+  // exprct: check the interval in the array whether overlap or not include in 0-20
+  // output: { overlap: Array, notInclude: Array };
+  const maxAge = 20;
+  const ArrayChecking = new Array(maxAge + 1).fill(0);
+  // 0: exclude, 1: picked, 2: overlap
+  intervals.forEach((i) => {
+    const start = parseInt(i[0]);
+    const end = parseInt(i[1]);
+    for (let j = start; j <= end; j++) {
+      if (ArrayChecking[j] === 0) ArrayChecking[j] = 1;
+      else if (ArrayChecking[j] === 1) ArrayChecking[j] = 2;
+    }
+  })
+
+  let overlap = [];
+  let overlapStart = null;
+  let notInclude = [];
+  let notIncludeStart = null;
+  for (let i = 0; i <= maxAge + 1; i++) {
+    if (overlapStart === null && notIncludeStart === null) {
+      if (ArrayChecking[i] === 0) notIncludeStart = i;
+      if (ArrayChecking[i] === 2) overlapStart = i;
+    } else {
+      if (overlapStart !== null && ArrayChecking[i] !== 2) {
+        overlap.push([overlapStart, i - 1]);
+        overlapStart = null;
+        if (ArrayChecking[i] === 0) notIncludeStart = i;
+      }
+      if (notIncludeStart !== null && ArrayChecking[i] !== 0) {
+        notInclude.push([notIncludeStart, i - 1]);
+        notIncludeStart = null;
+        if (ArrayChecking[i] === 2) overlapStart = i;
+      }
+    }
+  }
+
+  return { overlap, notInclude };
+}
